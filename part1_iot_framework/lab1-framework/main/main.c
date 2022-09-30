8e09895485ba3a15cdb5c0c5671fc957eeb4fcbc
baptiste <baptiste.dales@gmail.com>
2022-09-30 09:11:57 +0200

    add message in main

---
+++ part1_iot_framework/lab1-framework/main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Starting point function
+ * 
+ */
+
+void app_main(void) {
+
+	printf("Hello ESP32 !\n");
+	printf("New message !\n");
+	
+}
